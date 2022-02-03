8bcf36fac5edef5db5f7570dbc9ffac7d26eefb7
Yaroslava Yates <yaroslavayates@Yaroslavas-MBP.broadband>
2022-02-03 00:00:26 +0000

    Chapter 8 q1

--- chapter8/quiz1/questions/question_1.rb
+++ chapter8/quiz1/questions/question_1.rb
@@ -17,4 +17,21 @@
 #   contains letters and blanks.  You can assume the word will be in
 #   uppercase.
 
+puts "Enter a word"
+players_input = gets.chomp
 
+points = { " " => 0, "E" => 1, "A" => 1, "I" => 1, "O" => 1, 
+  "N" => 1, "R" => 1, "T" => 1, "L" => 1, "S" => 1, "U" => 1, 
+  "D" => 2, "G" => 2, "B" => 3, "C" => 3, "M" => 3, "P" => 3, 
+  "F" => 4, "H" => 4, "V" => 4, "W" => 4, "Y" => 4, "K" => 5, 
+  "J" => 8, "X" => 8, "Q" => 10, "Z" => 10 }
+
+word_score = 0
+
+letters = players_input.split("")
+
+letters.each do |letter|
+  word_score = word_score + points[letter]
+end
+
+puts word_score
